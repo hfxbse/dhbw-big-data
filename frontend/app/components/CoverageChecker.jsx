70e773f692323a2bb44156fa317b7f4dae411a93
Fabian Haas <29468630+hfxbse@users.noreply.github.com>
2024-11-17 18:34:50 +0100

    Fix missing marker icon in the production build

--- frontend/app/components/CoverageChecker.jsx
+++ frontend/app/components/CoverageChecker.jsx
@@ -2,6 +2,9 @@ import styles from './CoverageChecker.module.css';
 import Map from "./Map.client.jsx";
 import {ClientOnly} from "remix-utils/client-only";
 import 'leaflet/dist/leaflet.css'
+import 'leaflet/dist/images/marker-icon.png'
+import 'leaflet/dist/images/marker-icon-2x.png'
+import 'leaflet/dist/images/marker-shadow.png'
 
 
 export default function CoverageChecker({initialPosition, cellTowerCount, onLocationChange}) {
--- frontend/app/components/Map.client.jsx
+++ frontend/app/components/Map.client.jsx
@@ -1,6 +1,22 @@
 import {MapContainer, TileLayer, Marker, Popup, useMapEvents} from 'react-leaflet'
 import {useState, useEffect} from "react";
 
+import L from "leaflet";
+import markerIcon from "leaflet/dist/images/marker-icon.png";
+import markerIcon2x from "leaflet/dist/images/marker-icon-2x.png";
+import markerShadow from "leaflet/dist/images/marker-shadow.png";
+
+const MarkerIcon = L.icon({
+    iconUrl: markerIcon,
+    iconRetinaUrl: markerIcon2x,
+    shadowUrl: markerShadow,
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    tooltipAnchor: [16, -28],
+    shadowSize: [41, 41],
+});
+
 function LocationMarker({onChange, initialPosition}) {
     const [position, setPosition] = useState(null)
     const map = useMapEvents({
@@ -25,7 +41,7 @@ function LocationMarker({onChange, initialPosition}) {
     }, [])
 
     return position === null ? null : (
-        <Marker position={position}>
+        <Marker position={position} icon={MarkerIcon}>
             <Popup>You are here</Popup>
         </Marker>
     )
